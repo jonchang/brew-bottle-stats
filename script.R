424b5f2a51a805933fa64907310290a879116332
Jonathan Chang <jchang641@gmail.com>
2020-03-15 13:01:49 +1100

    Update script

--- script.R
+++ script.R
@@ -11,10 +11,12 @@ df2 <- gather(summed, "key", "value", bottled, total)
 
 df3 <- summed %>% mutate(pct = bottled / total)
 
-p1 <- ggplot(df2, aes(date, value, lty = key)) + geom_line() + labs(x = "Date", y = "Count") + scale_y_continuous(limits = c(0, 5100), expand = c(0, 0)) + scale_x_continuous(expand = c(0,0)) + theme_cowplot() + theme(legend.position = "none")
+p1 <- ggplot(df2, aes(date, value, lty = key)) + geom_line() + labs(x = "Date", y = "Count") + scale_y_continuous(limits = c(0, 5100), expand = c(0, 0)) + theme_cowplot() + theme(legend.position = "none")
 
 p2 <- ggplot(df3, aes(date, pct)) + geom_area(fill = "black") + labs(x = "Date", y = "Bottled") + scale_y_continuous(labels = scales::percent, limits = c(0, 0.75), expand = c(0, 0)) + theme_cowplot()
 
 svglite()
 plot_grid(p1, p2, ncol = 1)
 dev.off()
+
+write_csv(summed, "stats.csv")
